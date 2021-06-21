03035e4f1382ff454909faf868473b08b3f14da4
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-21 18:49:36 +0300

    Cart Screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CART_ADD_ITEM } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstants";
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
