b42919cda633a4f3089e97e6b3e90a9632572b93
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-21 19:10:47 +0300

    Remove Items From Cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -30,7 +30,7 @@ const CartScreen = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    console.log("remove");
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
