5a7f8d0e7cc00eda682f046219a0d898d6175931
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-21 18:03:05 +0300

    Add To Cart Functionalty

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -21,7 +21,7 @@ const HomeScreen = () => {
       {loading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error}</Message>
+        <Message variant="danger">{<h1>{error}</h1>}</Message>
       ) : (
         <Row>
           {products.map((product) => {
