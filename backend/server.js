e713f2a46a848a8dafa49ab8a5871eaa92418356
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-24 13:37:10 +0300

    Adding Paypal Payment

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,10 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+// PayPal
+app.get("/api/config/paypal", (req, res, next) => {
+  res.send(process.env.PAYPAL_CLIENT_ID);
+});
 
 app.use(notFound);
 app.use(errorHandler);
