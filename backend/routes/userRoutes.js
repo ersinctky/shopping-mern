9857d9501e0af399d808607d1ce767e095cbbbb7
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-24 16:08:08 +0300

    Admin User Delete

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,6 +6,7 @@ import {
   getUsers,
   registerUser,
   updateUserProfile,
+  deleteUser,
 } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
@@ -15,5 +16,7 @@ router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router.route("/:id").delete(protect, admin, deleteUser);
 
 export default router;
