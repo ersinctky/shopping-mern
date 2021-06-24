4cacbd21503b13717afb55590a5439aa1dc6a5b5
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-24 16:47:06 +0300

    User Edit Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import ShippingScreen from "./screens/ShippingScreen";
 import Paymentscreen from "./screens/PaymentScreen";
+import UserEditScreen from "./screens/UserEditScreen";
 
 const App = () => {
   return (
@@ -35,6 +36,7 @@ const App = () => {
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/admin/userlist" component={UserListScreen} />
+          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
 
           <Route path="/" component={HomeScreen} exact />
         </Container>
