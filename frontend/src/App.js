b59b859a91a8e2bc78002edf62252fde8a47b053
Ersin Çatkaya <ersin_ctky@hotmail.com>
2021-06-25 14:55:55 +0300

    Admin Order List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import Paymentscreen from "./screens/PaymentScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 const App = () => {
   return (
@@ -34,6 +35,7 @@ const App = () => {
           <Route path="/payment" component={Paymentscreen} />
           <Route path="/placeorder" component={PlaceOrderScreen} />
           <Route path="/order/:id" component={OrderScreen} />
+          <Route path="/admin/orderlist" component={OrderListScreen} />
 
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
